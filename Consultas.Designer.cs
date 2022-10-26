646e2dc94b5d69acc1318332d5f5eb0949db129c
NicoArroyo1 <84149390+NicoArroyo1@users.noreply.github.com>
2022-10-25 23:58:41 -0300

    primer comit

---
+++ Consultas.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Login
+{
+    partial class Consultas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvConsulta = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbConsulta1 = new System.Windows.Forms.Label();
+            this.lbConsulta2 = new System.Windows.Forms.Label();
+            this.lbConsulta3 = new System.Windows.Forms.Label();
+            this.lbConsulta4 = new System.Windows.Forms.Label();
+            this.lbConsulta5 = new System.Windows.Forms.Label();
+            this.lbConsulta6 = new System.Windows.Forms.Label();
+            this.lbConsulta7 = new System.Windows.Forms.Label();
+            this.lbConsulta8 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.lbConsulta9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConsulta
+            // 
+            this.dgvConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsulta.Location = new System.Drawing.Point(12, 342);
+            this.dgvConsulta.Name = "dgvConsulta";
+            this.dgvConsulta.RowTemplate.Height = 25;
+            this.dgvConsulta.Size = new System.Drawing.Size(1024, 226);
+            this.dgvConsulta.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Limpiar Consulta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Imprimir Consulta";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(925, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Volver al Menu";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Consulta 1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 78);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Consulta 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 107);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Consulta 3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 136);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Consulta 4";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 165);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Consulta 5";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 194);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Consulta 6";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 223);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Consulta 7";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 252);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Consulta 8";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 12;
+            // 
+            // lbConsulta1
+            // 
+            this.lbConsulta1.AutoSize = true;
+            this.lbConsulta1.Location = new System.Drawing.Point(93, 53);
+            this.lbConsulta1.Name = "lbConsulta1";
+            this.lbConsulta1.Size = new System.Drawing.Size(667, 15);
+            this.lbConsulta1.TabIndex = 13;
+            this.lbConsulta1.Text = "Se quiere saber la cantidad de automoviles vendidos en el año pasado junto con el" +
+    " precio y su promedio, que sean tipo Sedan";
+            this.lbConsulta1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbConsulta2
+            // 
+            this.lbConsulta2.AutoSize = true;
+            this.lbConsulta2.Location = new System.Drawing.Point(93, 82);
+            this.lbConsulta2.Name = "lbConsulta2";
+            this.lbConsulta2.Size = new System.Drawing.Size(728, 15);
+            this.lbConsulta2.TabIndex = 14;
+            this.lbConsulta2.Text = "Se desea saber el promedio de ventas realizadas de los meses del año pasados, que" +
+    " sean superior al promedio de ventas del mes corriente";
+            this.lbConsulta2.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbConsulta3
+            // 
+            this.lbConsulta3.AutoSize = true;
+            this.lbConsulta3.Location = new System.Drawing.Point(93, 111);
+            this.lbConsulta3.Name = "lbConsulta3";
+            this.lbConsulta3.Size = new System.Drawing.Size(463, 15);
+            this.lbConsulta3.TabIndex = 15;
+            this.lbConsulta3.Text = " Emitir un reporte de la cantidad de ventas realizada por tipo de cliente en el a" +
+    "ño actual";
+            this.lbConsulta3.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lbConsulta4
+            // 
+            this.lbConsulta4.AutoSize = true;
+            this.lbConsulta4.Location = new System.Drawing.Point(93, 136);
+            this.lbConsulta4.Name = "lbConsulta4";
+            this.lbConsulta4.Size = new System.Drawing.Size(793, 30);
+            this.lbConsulta4.TabIndex = 16;
+            this.lbConsulta4.Text = "Se desea obtener un listado de los clientes que hayan adquirido un autoplan en lo" +
+    "s ultimos 3 meses, con al menos una opcion de contacto conocida, \r\ny cuya cuota " +
+    "sea superior a 20000";
+            this.lbConsulta4.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lbConsulta5
+            // 
+            this.lbConsulta5.AutoSize = true;
+            this.lbConsulta5.Location = new System.Drawing.Point(93, 169);
+            this.lbConsulta5.Name = "lbConsulta5";
+            this.lbConsulta5.Size = new System.Drawing.Size(601, 15);
+            this.lbConsulta5.TabIndex = 17;
+            this.lbConsulta5.Text = "Listar las ventaS con sus respectivos empleados en el año actual las cuales super" +
+    "aron el promedio del año pasado";
+            this.lbConsulta5.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lbConsulta6
+            // 
+            this.lbConsulta6.AutoSize = true;
+            this.lbConsulta6.Location = new System.Drawing.Point(93, 194);
+            this.lbConsulta6.Name = "lbConsulta6";
+            this.lbConsulta6.Size = new System.Drawing.Size(598, 30);
+            this.lbConsulta6.TabIndex = 18;
+            this.lbConsulta6.Text = "Emitir un listado de los vehiculos que NO fueron vendidos este año. En ese listad" +
+    "o solo incluir aquellos vehículos\r\ncuyo precio este entre 1.000.000 y 1.500.000";
+            this.lbConsulta6.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lbConsulta7
+            // 
+            this.lbConsulta7.AutoSize = true;
+            this.lbConsulta7.Location = new System.Drawing.Point(93, 231);
+            this.lbConsulta7.Name = "lbConsulta7";
+            this.lbConsulta7.Size = new System.Drawing.Size(737, 15);
+            this.lbConsulta7.TabIndex = 19;
+            this.lbConsulta7.Text = "Emitir un listado en el que se comparten mensualmente el total de autos vendidos " +
+    "y total facturado en cada autoplan en el año 2022 y 2021";
+            this.lbConsulta7.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // lbConsulta8
+            // 
+            this.lbConsulta8.AutoSize = true;
+            this.lbConsulta8.Location = new System.Drawing.Point(93, 256);
+            this.lbConsulta8.Name = "lbConsulta8";
+            this.lbConsulta8.Size = new System.Drawing.Size(452, 15);
+            this.lbConsulta8.TabIndex = 20;
+            this.lbConsulta8.Text = "Emitir un reporte de la cantidad de ventas del año pasado segun el tipo del vehic" +
+    "ulos";
+            this.lbConsulta8.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(12, 281);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 21;
+            this.button12.Text = "Consulta 9";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // lbConsulta9
+            // 
+            this.lbConsulta9.AutoSize = true;
+            this.lbConsulta9.Location = new System.Drawing.Point(93, 285);
+            this.lbConsulta9.Name = "lbConsulta9";
+            this.lbConsulta9.Size = new System.Drawing.Size(596, 15);
+            this.lbConsulta9.TabIndex = 22;
+            this.lbConsulta9.Text = "Emitir un reporte de las 5 autopartes mas vendidas de cierto año, cuyo precio est" +
+    "e por encima de cierto numero.";
+            this.lbConsulta9.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Consultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 580);
+            this.Controls.Add(this.lbConsulta9);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.lbConsulta8);
+            this.Controls.Add(this.lbConsulta7);
+            this.Controls.Add(this.lbConsulta6);
+            this.Controls.Add(this.lbConsulta5);
+            this.Controls.Add(this.lbConsulta4);
+            this.Controls.Add(this.lbConsulta3);
+            this.Controls.Add(this.lbConsulta2);
+            this.Controls.Add(this.lbConsulta1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvConsulta);
+            this.Name = "Consultas";
+            this.Text = "Consultas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgvConsulta;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Label label1;
+        private Label lbConsulta1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Button button12;
+        private Label label10;
+        private Label lbConsulta2;
+        private Label lbConsulta3;
+        private Label lbConsulta4;
+        private Label lbConsulta5;
+        private Label lbConsulta6;
+        private Label lbConsulta7;
+        private Label lbConsulta8;
+        private Label lbConsulta9;
+    }
+}
---
+++ SQLControl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Login
+{
+    class SQLControl
+    {
+        private string conexionString;
+        private SqlConnection cnn;
+        private SqlCommand cmd;
+        private DataTable table;
+
+        public SQLControl()
+        {
+            conexionString = @"Data Source=DESKTOP-DIVRQKS\SERVIDORSQL;Initial Catalog=VIVERO_FENIX;Integrated Security=True";
+            cnn = new SqlConnection(conexionString);
+        }
+        public DataTable ConsultarSQL(string query) 
+        {
+            cnn.Open();
+            cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = cnn;
+            table = new DataTable();
+            table.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return table;
+        }
+        
+
+        public int Login (string usario, string pass) 
+        {
+            try
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand("spLogin", cnn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@usuario", usario);
+                cmd.Parameters.AddWithValue("@pass", pass);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    return dr.GetInt32(0);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally 
+            {
+                cnn.Close();
+            }
+            return -1;
+
+
+        }
+
+    }
+}
